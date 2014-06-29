db60dd810784a9737a520351005eb0dce2e8996b
Markus Hauck <markus1189@gmail.com>
2014-06-29 10:08:10 +0200

    Basic traits for sig expr fusion

---
+++ src/main/scala/rescala/lms/optimizations/SigExprFusion.scala
@@ -0,0 +1,30 @@
+package rescala.lms.optimizations
+
+import scala.virtualization.lms.common.{Base, EffectExp, ScalaGenEffect, FunctionsExp}
+import scala.virtualization.lms.internal.Expressions
+import language.implicitConversions
+import rescala.lms.syntaxops._
+
+import rescala.{Signal => RESignal, SignalSynt => RESignalSynt, DepHolder}
+
+/** Fuse signal expressions */
+trait SigExprFusionSyntax extends Base {
+  object SignalF {
+    def apply[A:Manifest](deps: Rep[DepHolder]*)(expr: Rep[RESignalSynt[A]] => Rep[A]) =
+      sig_expr_fusion_newSignal(deps,expr)
+  }
+
+  def sig_expr_fusion_newSignal[A:Manifest](deps: Seq[Rep[DepHolder]],
+    expr: Rep[RESignalSynt[A]] => Rep[A]): Rep[RESignalSynt[A]]
+}
+
+trait SigExprFusionOps extends SigExprFusionSyntax {
+  this: SignalOps =>
+
+  override def sig_expr_fusion_newSignal[A:Manifest](deps: Seq[Rep[DepHolder]],
+    expr: Rep[RESignalSynt[A]] => Rep[A]): Rep[RESignalSynt[A]] = deps match {
+    case Seq() => ???
+    case Seq(d) => ???
+    case _ => sig_ops_newSignal(deps,expr)
+  }
+}
