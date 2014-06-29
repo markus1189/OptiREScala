e57568715e847b9d363e9f4756656b2429ab6ca2
Markus Hauck <markus1189@gmail.com>
2014-06-29 09:38:41 +0200

    Add map fusion test with intermediate assignments

--- src/test/scala/rescala/lms/dsl/MapFusionSpec.scala
+++ src/test/scala/rescala/lms/dsl/MapFusionSpec.scala
@@ -26,6 +26,24 @@ class MapFusionSpec extends WordSpec with Matchers {
       prog.codegen.emitSource(prog.f, "F", new java.io.PrintWriter(out))
       out.toString.lines.toList.filter(_.contains(".map")) should have length(1)
     }
+
+    "fuse even if intermediate assignments are used" in {
+      val prog = new MapFusionAssignProg with ReactiveDSLExp with CompileScala { self =>
+        override val codegen = new ReactiveDSLGen {
+          val IR: self.type = self
+        }
+      }
+
+      val s = prog.compile(prog.f).apply( () )
+
+      // Check the result value
+      s.get should equal(1+1+2+3)
+
+      // Check result of code generation: only one call to map
+      val out = new java.io.StringWriter();
+      prog.codegen.emitSource(prog.f, "F", new java.io.PrintWriter(out))
+      out.toString.lines.toList.filter(_.contains(".map")) should have length(1)
+    }
   }
 }
 
@@ -33,5 +51,16 @@ trait MapFusionProg extends ReactiveDSL {
   def f(x : Rep[Unit]) = {
     val v: Rep[RESignal[Int]] = Signal() { s: Rep[RESignalSynt[Int]] => 1}
     v.fuseMap(_+1).fuseMap(_+2).fuseMap(_+3)
+  }
+}
+
+trait MapFusionAssignProg extends ReactiveDSL {
+  def f(x: Rep[Unit]) = {
+    val v: Rep[RESignal[Int]] = Signal() { s: Rep[RESignalSynt[Int]] => 1}
+    val m1 = v.fuseMap(_+1)
+    val m2 = m1.fuseMap(_+2)
+    val m3 = m2.fuseMap(_+3)
+
+    m3
   }
 }
