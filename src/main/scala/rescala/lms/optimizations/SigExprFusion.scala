ce36782fc309c3535218572f6f0465af7d171a58
Markus Hauck <markus1189@gmail.com>
2014-07-11 11:23:28 +0200

    Remove obsolete traits in SigExprFusion

--- src/main/scala/rescala/lms/optimizations/SigExprFusion.scala
+++ src/main/scala/rescala/lms/optimizations/SigExprFusion.scala
@@ -8,28 +8,6 @@ import rescala.lms._
 
 import rescala.{Signal => RESignal, SignalSynt => RESignalSynt, DepHolder}
 
-/** Fuse signal expressions */
-trait SigExprFusionSyntax extends Base {
-  object SignalF {
-    def apply[A:Manifest](deps: Rep[DepHolder]*)(expr: Rep[RESignalSynt[A]] => Rep[A]) =
-      sig_expr_fusion_newSignal(deps,expr)
-  }
-
-  def sig_expr_fusion_newSignal[A:Manifest](deps: Seq[Rep[DepHolder]],
-    expr: Rep[RESignalSynt[A]] => Rep[A]): Rep[RESignalSynt[A]]
-}
-
-trait SigExprFusionOps extends SigExprFusionSyntax {
-  this: SignalOps =>
-
-  override def sig_expr_fusion_newSignal[A:Manifest](deps: Seq[Rep[DepHolder]],
-    expr: Rep[RESignalSynt[A]] => Rep[A]): Rep[RESignalSynt[A]] = deps match {
-    case Seq() => ??? /** TODO: no dependencies, simple subst? */
-    case Seq(d) => ??? /** TODO: one dependency, try to inline? */
-    case _ => sig_ops_newSignal(deps,expr)
-  }
-}
-
 object FusionTransformers {
 
   // sig.apply(sig2) --> sig.apply()
