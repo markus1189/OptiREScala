b4ce3dbfbe7b8f0a6ed1724807ac43cd79f2fb7e
Markus Hauck <markus1189@gmail.com>
2014-07-11 11:21:53 +0200

    Document sigApplyDepTransformer

--- src/main/scala/rescala/lms/optimizations/SigExprFusion.scala
+++ src/main/scala/rescala/lms/optimizations/SigExprFusion.scala
@@ -31,12 +31,13 @@ trait SigExprFusionOps extends SigExprFusionSyntax {
 }
 
 object FusionTransformers {
+
+  // sig.apply(sig2) --> sig.apply()
   def sigApplyDepTransformer(prog: CompileScala with ReactiveDSLExp) = {
     new ForwardTransformer {
       val IR: prog.type = prog
       import IR._
       override def transformStm(stm: Stm) = {
-        // System.out.println(stm)
         stm match {
           case TP(s,Reflect(a@SigApplyDep(s1,s2),t,u)) =>   // x1(x2) --> x1()
             sig_ops_apply(s1)(a.t)
