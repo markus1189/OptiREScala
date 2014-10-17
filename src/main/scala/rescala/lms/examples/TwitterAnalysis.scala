76e060303e14302f03e30ae7515510e7625b4c12
Markus Hauck <markus1189@gmail.com>
2014-10-17 13:21:51 +0200

    Word cloud for REScala

--- src/main/scala/rescala/lms/examples/TwitterAnalysis.scala
+++ src/main/scala/rescala/lms/examples/TwitterAnalysis.scala
@@ -15,9 +15,9 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
 
-object TwitterAnalysis extends App {
-  val BATCH_SIZE = 100
-  val SLEEP_DURATION = 1000
+object TwitterAnalysis extends SimpleSwingApplication {
+  val BATCH_SIZE = 1000
+  val SLEEP_DURATION = 125
 
   val tweetEvent = new ImperativeEvent[String]
 
@@ -27,63 +27,67 @@ object TwitterAnalysis extends App {
 
   val processedTweets =
     tweetsInput.
-      map(Functions.lowercase).
       map(Functions.toWords).
-      map(Functions.countWords).
-      map(Functions.sumIt)
+      map(_.flatten).
+      map(Functions.lowercase).
+      // stopwords
+      // stemming
+      map(Functions.wordFrequency).
+      map(_.filter { case (word,weight) => weight > 1 }).
+      map(Functions.normalizeFrequency).
+      map(_.toSeq).
+      map(Functions.createLabels)
 
-  processedTweets.changed += { x => println(x) }
+  def top = new MainFrame { // top is a required method
+    title = "Twitter Word Cloud"
+
+    val panel = new FlowPanel
+
+    processedTweets.changed += { labels =>
+      panel.contents.clear
+      panel.contents ++= labels
+      panel.revalidate()
+      panel.repaint()
+    }
+
+    contents = panel
+    size = new Dimension(300, 200)
+  }
 
   val fut = future {
-    val (batch,remaining) = tweets.splitAt(BATCH_SIZE)
-    batch.foreach(tweetEvent(_))
-
-    remaining.foreach { tweet =>
+    tweets.foreach { tweet =>
       Thread.sleep(SLEEP_DURATION)
       tweetEvent(tweet)
     }
   }
-
-  Await.ready(fut, Duration.Inf)
 }
 
 object Functions {
-  val lowercase: Seq[String] => Seq[String] = seq => seq.map(_.map(_.toLower))
+  val lowercase: Seq[String] => Seq[String] = _.map(_.map(_.toLower))
+
   val toWords: Seq[String] => Seq[Seq[String]] = _.map(_.split("\\s+").toSeq)
+
   def countWords[A]: Seq[Seq[A]] => Seq[Int] = seq => seq.map(_.length)
+
   val sumIt: Seq[Int] => Int = _.sum
-}
 
-object SimpleGUI extends SimpleSwingApplication {
-  def top = new MainFrame { // top is a required method
-    title = "Twitter Word Cloud"
+  val randomWeights: Seq[String] => Seq[(String,Int)] =
+    _.map { w => (w,scala.util.Random.nextInt(10))}
 
-    val words = Seq("berzelius","intrepidly","wren","salivary","tar","straplesses","williamson","servitude","snowdrift","tigers","sumach","exile","molnar","sandbag","semester","japanese","faithful","blunders","freights","falsetto","dewy","pernod","scrambler","conventionality","forenames","hackneying","bust","attar","johnathan","spontaneous","elaboration","cubicles","tianjin","spicy","funerals","cosmologist","smacker","isuzu","klondikes","registrar","landmass","caribou","micky","walkout","connolly","shrieks","medicaids","archway","jersey","brain","beetle","viva","furriest","sinewy","witting","butterfingers","gentry","seeding","underwear","barrelled","brooke","redcoats","extravert","mutual","inferior","timidest","incidentally","virtue","designation","acquaint","yuck","palaces","regal","grommets","copernicus","excommunications","inalienable","consequence","elongates","barrenness","chorus","grommet","agile","chichi","episcopalians","bathmat","mousiness","rheumier","wilson","guppy","selector","details","thornton","lean","parker","puncture","anorexia","hals","proscribe")
-
-    def randomWeights(words: Seq[String]): Seq[(String,Int)] = words.map { w => (w,scala.util.Random.nextInt(50))}
-
-    def createLabels(wordsWithWeight: Seq[(String,Int)]): Seq[Label] =
-      wordsWithWeight.map { case (word,weight) =>
-        val label = new Label(word)
-        label.font = new Font(label.font.getName, Font.PLAIN, weight)
-        label
-      }
-
-    val fromWords = (createLabels _).compose(randomWeights _)
-
-    val panel = new FlowPanel { contents ++= fromWords(words)}
-
-    contents = panel
-    size = new Dimension(300, 200)
-
-    future {
-      while (true) {
-        Thread.sleep(5000)
-        panel.contents.clear()
-        panel.contents ++= fromWords(words)
-        Thread.sleep(1000)
-        panel.revalidate()
-      }
+  val wordFrequency: Seq[String] => Map[String,Int] =
+    _.foldLeft(Map[String,Int]().withDefaultValue(0)) { (acc,w) =>
+      acc + (w -> (acc(w) + 1))
     }
+
+  val normalizeFrequency: Map[String,Int] => Map[String,Float] = freqMap => {
+    val maxFreq = if (freqMap.isEmpty) 1 else freqMap.values.max
+    freqMap.mapValues { x => x.toFloat / maxFreq }
   }
+
+  val createLabels: Seq[(String,Float)] => Seq[Label] = wordsWithWeight =>
+    wordsWithWeight.map { case (word,weight) =>
+      val label = new Label(word)
+      label.font = new Font(label.font.getName, Font.PLAIN, (weight*100).toInt)
+      label
+    }
 }
