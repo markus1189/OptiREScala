97bcbdd7fbb7c698c036f7cb2903c4d893b93920
Markus Hauck <markus1189@gmail.com>
2014-10-26 16:14:35 +0100

    Fix travis

--- travis/build.sh
+++ travis/build.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+echo "Building in '${pwd}'"
+
+ls -A
+
 sbt compile
 sbt test
+
+echo "Done"
--- travis/install-rescala.sh
+++ travis/install-rescala.sh
@@ -9,8 +9,9 @@ fi
 cd "dependencies"
 
 git clone "https://github.com/guidosalva/REScala.git"
+cd "REScala"
+
 # We need a version that works with Scala 2.10
 git reset --hard abb2d0cf5de58f404baac2f1b72b547635444d58
-cd "REScala"
 
 sbt compile
